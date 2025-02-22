70da1a3adea7ac4a00ad8a85f03bc1aea1ca447f
queensheba <q.oluwadare@centrifugegroup.com>
2025-02-22 06:14:44 +0100

    newly built

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  open_ai_key: 'sk-proj-EO04uJDi5nhdT514JJbMT3BlbkFJ3J7lyckmVRg5YvULu1uR',
+  pineconeKey: 'pcsk_4uJ3ZH_4PSNZmez4CL34tDzqFBsqHdt4T2UP2EJpEjcHsfP8oAf4SrV4uX3FZ7FpGJ1vis',
+  pineconeIndex: 'jamb-chatbot'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+
+
+export const environment = {
+    production: false,
+    openaiApiKey: 'sk-proj-EO04uJDi5nhdT514JJbMT3BlbkFJ3J7lyckmVRg5YvULu1uR',
+    pineconeKey: 'pcsk_4uJ3ZH_4PSNZmez4CL34tDzqFBsqHdt4T2UP2EJpEjcHsfP8oAf4SrV4uX3FZ7FpGJ1vis',
+    pineconeIndex: 'jamb-chatbot'
+};
